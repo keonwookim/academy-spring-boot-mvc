96d8b97a7501dd7a71965d7e453eb0c7be362cc8
김건우/Dooray개발실 <keonwoo.kim@nhndooray.com>
2024-04-25 15:58:33 +0900

    initial commit

---
+++ src/main/java/com/nhnacademy/springbootmvc/AcademySpringBootMvcApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.springbootmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademySpringBootMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcademySpringBootMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhnacademy/springbootmvc/AcademySpringBootMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.springbootmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademySpringBootMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
