2cce3aa44c1ab08bef4401b97f5af2c4257f7618
김건우/Dooray개발실 <keonwoo.kim@nhndooray.com>
2024-05-13 17:39:44 +0900

    UserRepository, UserRepositoryImpl 메서드 추가

--- src/main/java/com/nhnacademy/springbootmvc/repository/UserRepositoryImpl.java
+++ src/main/java/com/nhnacademy/springbootmvc/repository/UserRepositoryImpl.java
@@ -3,10 +3,7 @@ package com.nhnacademy.springbootmvc.repository;
 import com.nhnacademy.springbootmvc.domain.User;
 import org.springframework.stereotype.Repository;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 @Repository
 public class UserRepositoryImpl implements UserRepository {
@@ -16,6 +13,18 @@ public class UserRepositoryImpl implements UserRepository {
         userMap.put("1", User.create("Tom", "1234"));
         userMap.put("2", User.create("Jake", "5678"));
         userMap.put("3", User.create("Ethan", "1010"));
+    }
+
+    @Override
+    public boolean exists(String id) {
+        return userMap.containsKey(id);
+    }
+
+    @Override
+    public boolean matches(String id, String password) {
+        return Optional.ofNullable(getUser(id))
+                .map(user -> user.getPassword().equals(password))
+                .orElse(false);
     }
 
     @Override
