7041dc34d6856f086ed08d189b9afbe5fcd97e2b
김건우/Dooray개발실 <keonwoo.kim@nhndooray.com>
2024-05-13 17:38:32 +0900

    login2 todo 구현

--- src/main/java/com/nhnacademy/springbootmvc/repository/UserRepository.java
+++ src/main/java/com/nhnacademy/springbootmvc/repository/UserRepository.java
@@ -5,6 +5,8 @@ import com.nhnacademy.springbootmvc.domain.User;
 import java.util.List;
 
 public interface UserRepository {
+    boolean exists(String id);
+    boolean matches(String id, String password);
     List<User> getUsers();
     User getUser(String id);
 }
