a15b31621bc36a60fd67b664245bb218d304b784
김건우/Dooray개발실 <keonwoo.kim@nhndooray.com>
2024-05-14 01:57:18 +0900

    파일 업로드 추가

--- src/main/java/com/nhnacademy/springbootmvc/config/WebConfig.java
+++ src/main/java/com/nhnacademy/springbootmvc/config/WebConfig.java
@@ -4,8 +4,11 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
@@ -15,13 +18,11 @@ import java.util.Locale;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    // TODO #1: `LocaleResolver` 설정
     @Bean
     public LocaleResolver localeResolver() {
         return new FixedLocaleResolver(Locale.KOREAN);
     }
 
-    // TODO #4: `MessageSource` 빈 설정 - 다국어 지원.
     @Bean
     public MessageSource messageSource() {
         ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
@@ -32,10 +33,20 @@ public class WebConfig implements WebMvcConfigurer {
         return messageSource;
     }
 
-    // TODO #2: `LocaleChangeInterceptor` 추가
-    //         `locale`이라는 파라미터로 전달된 값으로 locale을 변경.
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LocaleChangeInterceptor());
+    }
+
+    // TODO #2: MultipartResolver 빈 설정
+    @Bean
+    public MultipartResolver multipartResolver() {
+        return new StandardServletMultipartResolver();
+    }
+
+    // TODO #3: upload form 설정
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/upload").setViewName("upload");
     }
 }
