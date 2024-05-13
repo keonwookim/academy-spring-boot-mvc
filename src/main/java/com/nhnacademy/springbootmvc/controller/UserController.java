d68776e23e32f42aad9d74faec1edfc34f8d5b4c
김건우/Dooray개발실 <keonwoo.kim@nhndooray.com>
2024-05-14 00:10:17 +0900

    게시물 등록 구현

--- src/main/java/com/nhnacademy/springbootmvc/controller/UserController.java
+++ src/main/java/com/nhnacademy/springbootmvc/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.nhnacademy.springbootmvc.controller;
 
 import com.nhnacademy.springbootmvc.domain.User;
+import com.nhnacademy.springbootmvc.exception.UserNotFoundException;
 import com.nhnacademy.springbootmvc.repository.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
+import java.util.Objects;
 
 @Controller
 public class UserController {
@@ -28,7 +30,23 @@ public class UserController {
     public String getUser(Model model,
                           @PathVariable("userId") String id) {
         User user = userRepository.getUser(id);
+        if (Objects.isNull(user)) {
+            model.addAttribute("exception", new UserNotFoundException());
+            return "error";
+        }
         model.addAttribute("user", user);
         return "user";
+    }
+
+    @GetMapping("/{userId}/modify")
+    public String userModifyForm(@PathVariable("userId") String userId, Model model) {
+        User user = userRepository.getUser(userId);
+        if (Objects.isNull(user)) {
+            model.addAttribute("exception", new UserNotFoundException());
+            return "error";
+        }
+
+        model.addAttribute("user", user);
+        return "userModify";
     }
 }
---
+++ src/main/java/com/nhnacademy/springbootmvc/exception/PostNotFoundException.java
@@ -0,0 +1,4 @@
+package com.nhnacademy.springbootmvc.exception;
+
+public class PostNotFoundException extends RuntimeException {
+}
