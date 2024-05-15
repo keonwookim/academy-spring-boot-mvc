25abb541a20591baf4f97711db890931d6ae0bf3
김건우/Dooray개발실 <keonwoo.kim@nhndooray.com>
2024-05-15 10:19:33 +0900

    user map 수정

--- src/main/java/com/nhnacademy/springbootmvc/repository/UserRepositoryImpl.java
+++ src/main/java/com/nhnacademy/springbootmvc/repository/UserRepositoryImpl.java
@@ -12,9 +12,9 @@ public class UserRepositoryImpl implements UserRepository {
     private final Map<String, User> userMap = new HashMap<>();
 
     public UserRepositoryImpl() {
-        userMap.put("1", User.create("1", "Tom"));
-        userMap.put("2", User.create("2", "Jake"));
-        userMap.put("3", User.create("3", "Ethan"));
+        userMap.put("1", User.create("tom", "1234"));
+        userMap.put("2", User.create("jake", "5678"));
+        userMap.put("3", User.create("ethan", "1010"));
     }
 
     @Override
