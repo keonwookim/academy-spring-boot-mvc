f72c0e68c214439d1d3301e8c44fb8d0938daa78
김건우/Dooray개발실 <keonwoo.kim@nhndooray.com>
2024-05-15 10:00:03 +0900

    thymeleaf 추가

---
+++ src/main/java/com/nhnacademy/springbootmvc/controller/ThymeleafController.java
@@ -0,0 +1,25 @@
+package com.nhnacademy.springbootmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+public class ThymeleafController {
+    @GetMapping("/thymeleaf")
+    public String thymeleaf(Model model) {
+        model.addAttribute("greeting", "Hello");
+
+        model.addAttribute("html", "this is <b>HTML</b>.");
+
+        List<String> list = Arrays.asList("nhn", "academy", "dooray");
+        model.addAttribute("list", list);
+
+        model.addAttribute("choice", 2);
+
+        return "thymeleaf";
+    }
+}
