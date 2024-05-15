55297fd8185806e3d2ce88df931f7dd86cac9d51
김건우/Dooray개발실 <keonwoo.kim@nhndooray.com>
2024-05-15 11:09:32 +0900

    fix bugs

--- src/main/java/com/nhnacademy/springbootmvc/repository/UserRepositoryImpl.java
+++ src/main/java/com/nhnacademy/springbootmvc/repository/UserRepositoryImpl.java
@@ -13,9 +13,9 @@ public class UserRepositoryImpl implements UserRepository {
     private final Map<String, User> userMap = new HashMap<>();
 
     public UserRepositoryImpl() {
-        userMap.put("1", User.create("tom", "1234"));
-        userMap.put("2", User.create("jake", "5678"));
-        userMap.put("3", User.create("ethan", "1010"));
+        userMap.put("tom", User.create("tom", "1234"));
+        userMap.put("jake", User.create("jake", "5678"));
+        userMap.put("ethan", User.create("ethan", "1010"));
     }
 
     @Override
