b5c4cba77e004fec9f30ba1769fca85285293cd9
김건우/Dooray개발실 <keonwoo.kim@nhndooray.com>
2024-05-19 21:45:14 +0900

    multi viewresolver

--- src/main/java/com/nhnacademy/springbootmvc/controller/HomeController.java
+++ src/main/java/com/nhnacademy/springbootmvc/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.nhnacademy.springbootmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,5 +10,11 @@ public class HomeController {
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/mustache")
+    public String mustacheIndex(Model model) {
+        model.addAttribute("message", "Hello World!");
+        return "mustache/index";
     }
 }
