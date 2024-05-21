689b60f35b5bbdf4fd748661df880429c1fef971
김건우/Dooray개발실 <keonwoo.kim@nhndooray.com>
2024-05-22 02:08:20 +0900

    fix

--- src/main/java/com/nhnacademy/springbootmvc/controller/UserRegisterController.java
+++ src/main/java/com/nhnacademy/springbootmvc/controller/UserRegisterController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/users/register")
+@RequestMapping("/user/register")
 public class UserRegisterController {
     private final UserRepository userRepository;
 
