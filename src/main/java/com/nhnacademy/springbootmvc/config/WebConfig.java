b685342f00e0620a940e6d0ec013eeaed639d39c
김건우/Dooray개발실 <keonwoo.kim@nhndooray.com>
2025-05-19 22:49:32 +0900

    fix a bug

--- src/main/java/com/nhnacademy/springbootmvc/config/WebConfig.java
+++ src/main/java/com/nhnacademy/springbootmvc/config/WebConfig.java
@@ -2,8 +2,10 @@ package com.nhnacademy.springbootmvc.config;
 
 import com.nhnacademy.springbootmvc.converter.StringToRoleConverter;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Component
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry) {
--- src/main/java/com/nhnacademy/springbootmvc/converter/StringToRoleConverter.java
+++ src/main/java/com/nhnacademy/springbootmvc/converter/StringToRoleConverter.java
@@ -4,7 +4,7 @@ import com.nhnacademy.springbootmvc.model.Role;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
-@Component
+
 public class StringToRoleConverter implements Converter<String, Role> {
     @Override
     public Role convert(String source) {
